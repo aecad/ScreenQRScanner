81bed829b5ce97c18066fc9aeb04f70326b23bd2
aecad <111349557+aecad@users.noreply.github.com>
2026-06-01 17:59:36 +0800

    Add material-icons-extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
     // ML Kit for QR code scanning
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
     
